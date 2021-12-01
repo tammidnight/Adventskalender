98e173999fe644d19729b7ec9ff59969a38eb062
Tamara Rott <tam.midnight@gmail.com>
2021-12-01 12:52:09 +0100

    done

---
+++ src/script.js
@@ -0,0 +1,201 @@
+window.addEventListener("load", () => {
+  image.style.display = "none";
+
+  let today = new Date();
+
+  image.addEventListener("click", () => {
+    image.style.display = "none";
+  });
+
+  day01.addEventListener("click", () => {
+    if (today >= new Date("2021/12/01")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/01.png" alt="day01"></div>';
+    }
+  });
+
+  day02.addEventListener("click", () => {
+    if (today >= new Date("2021/12/02")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/02.jpeg" alt="day02"></div>';
+    }
+  });
+
+  day03.addEventListener("click", () => {
+    if (today >= new Date("2021/12/03")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/03.png" alt="day03"></div>';
+    }
+  });
+
+  day04.addEventListener("click", () => {
+    if (today >= new Date("2021/12/04")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/04.jpg" alt="day04"></div>';
+    }
+  });
+
+  day05.addEventListener("click", () => {
+    if (today >= new Date("2021/12/05")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/05.png" alt="day05"></div>';
+    }
+  });
+
+  day06.addEventListener("click", () => {
+    if (today >= new Date("2021/12/06")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/06.jpeg" alt="day06"></div>';
+    }
+  });
+
+  day07.addEventListener("click", () => {
+    if (today >= new Date("2021/12/07")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/07.png" alt="day07"></div>';
+    }
+  });
+
+  day08.addEventListener("click", () => {
+    if (today >= new Date("2021/12/08")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/08.jpg" alt="day08"></div>';
+    }
+  });
+
+  day09.addEventListener("click", () => {
+    if (today >= new Date("2021/12/09")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/09.png" alt="day09"></div>';
+    }
+  });
+
+  day10.addEventListener("click", () => {
+    if (today >= new Date("2021/12/10")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/10.png" alt="day10"></div>';
+    }
+  });
+
+  day11.addEventListener("click", () => {
+    if (today >= new Date("2021/12/11")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/11.png" alt="day11"></div>';
+    }
+  });
+
+  day12.addEventListener("click", () => {
+    if (today >= new Date("2021/12/12")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/12.jpg" alt="day12"></div>';
+    }
+  });
+
+  day13.addEventListener("click", () => {
+    if (today >= new Date("2021/12/13")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/13.png" alt="day13"></div>';
+    }
+  });
+
+  day14.addEventListener("click", () => {
+    if (today >= new Date("2021/12/14")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/14.jpg" alt="day14"></div>';
+    }
+  });
+
+  day15.addEventListener("click", () => {
+    if (today >= new Date("2021/12/15")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/15.jpg" alt="day15"></div>';
+    }
+  });
+
+  day16.addEventListener("click", () => {
+    if (today >= new Date("2021/12/16")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/16.png" alt="day16"></div>';
+    }
+  });
+
+  day17.addEventListener("click", () => {
+    if (today >= new Date("2021/12/17")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/17.png" alt="day17"></div>';
+    }
+  });
+
+  day18.addEventListener("click", () => {
+    if (today >= new Date("2021/12/18")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/18.jpg" alt="day18"></div>';
+    }
+  });
+
+  day19.addEventListener("click", () => {
+    if (today >= new Date("2021/12/19")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/19.JPG" alt="day19"></div>';
+    }
+  });
+
+  day20.addEventListener("click", () => {
+    if (today >= new Date("2021/12/20")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/20.jpg" alt="day20"></div>';
+    }
+  });
+
+  day21.addEventListener("click", () => {
+    if (today >= new Date("2021/12/21")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/21.png" alt="day21"></div>';
+    }
+  });
+
+  day22.addEventListener("click", () => {
+    if (today >= new Date("2021/12/22")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/22.jpg" alt="day22"></div>';
+    }
+  });
+
+  day23.addEventListener("click", () => {
+    if (today >= new Date("2021/12/23")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/23.png" alt="day23"></div>';
+    }
+  });
+
+  day24.addEventListener("click", () => {
+    if (today >= new Date("2021/12/24")) {
+      image.style.display = "block";
+      image.innerHTML =
+        '<div id="image"><img src="./images/days/24.png" alt="day24"></div>';
+    }
+  });
+});
